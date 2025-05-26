03a066e644611c823c4629f93bc00638b582617a
ASCDarshan <ascdarshan@gmail.com>
2025-05-26 14:49:59 +0530

    remove fonts

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,6 @@ import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/600.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/open-sans/300.css";
-import "@fontsource/open-sans/400.css";
-import "@fontsource/open-sans/500.css";
-import "@fontsource/open-sans/600.css";
-import "@fontsource/open-sans/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
